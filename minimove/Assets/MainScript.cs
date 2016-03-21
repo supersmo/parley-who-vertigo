3d4f2c60307097a37eeeed468a7abffef57a22c3
Thomas Perl <m@thp.io>
2016-03-21 10:18:50 +0100

    Setup Unity project

---
+++ minimove/Assets/MainScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Update");
+	
+	}
+}
