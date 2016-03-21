e856d7fd719302f155a11fe3810b2eb42909c823
Thomas Perl <m@thp.io>
2016-03-21 10:43:54 +0100

    Connect to two controllers

--- minimove/Assets/MainScript.cs
+++ minimove/Assets/MainScript.cs
@@ -2,15 +2,30 @@
 using System.Collections;
 
 public class MainScript : MonoBehaviour {
+	private GameObject moveObject = null;
+	private UniMoveController move = null;
+	private UniMoveController move2 = null;
 
 	// Use this for initialization
 	void Start () {
+		moveObject = GameObject.Find ("MoveObject");
+		move = moveObject.AddComponent<UniMoveController> ();
+		move2 = moveObject.AddComponent<UniMoveController> ();
+
 		Debug.Log ("Hello start");
+		int count = UniMoveController.GetNumConnected ();
+		Debug.Log ("Controllers Connected: " + count);
+		if (move.Init (0) && move2.Init(1)) {
+			Debug.Log("Initialized successfully");
+		} else {
+			Debug.Log("Failed to initialize");
+		}			
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("Update");
-	
+		//Debug.Log ("Update");
+		move.SetLED(Color.cyan);
+		move2.SetLED (Color.red);
 	}
 }
