f688885c3d4e3cd510adc7384a25b2d6091a253f
Thomas Perl <m@thp.io>
2016-07-14 15:44:00 +0200

    Minimize freeze.lua

--- freeze.lua
+++ freeze.lua
@@ -2,23 +2,16 @@ freezing_blue = color(0.0, 0.4, 0.9)
 
 function intro()
     for i=1,4 do
-        sfx(cycle_blip)
-        led(freezing_blue)
-        wait(0.4)
-
-        sfx(cycle_blip)
-        led(off)
-        wait(0.2)
+        sfx(cycle_blip) led(freezing_blue) wait(0.4)
+        sfx(cycle_blip) led(off) wait(0.2)
     end
 
-    sfx(beep)
-    color(FreezingBlue)
+    sfx(beep) led(freezing_blue)
 end
 
 function gameplay()
     if player.alive and player.is_unstable then
-        sfx(balloon_explosion)
-        player.alive = false
+        sfx(balloon_explosion) player.alive = false
     end
 
     led(player.alive and freezing_blue or off)
