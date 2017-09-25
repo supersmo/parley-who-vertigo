2ddd8be130fd67c7fff0d69d8a1492aaa1edf186
Thomas Perl <m@thp.io>
2017-09-25 17:14:00 +0200

    Update packaging script

--- bundle/package.sh
+++ bundle/package.sh
@@ -1,15 +1,83 @@
 #!/bin/bash
+# Package Parley Who Vertigo
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 SRC="$DIR/.."
-DEST="$DIR/Parley Who Vertigo.app"
-rm -rf "$DEST"
-mkdir -p "$DEST/Contents/MacOS" "$DEST/Contents/Resources"
-cp "$SRC/../tools/psmovepair" "$DIR"
-cp $SRC/../docs/*.pdf "$DIR"
-cp "$DIR/Info.plist" "$DEST/Contents"
-cp "$DIR/parleywhovertigo.icns" "$DEST/Contents/Resources"
-cp -rpv "$SRC"/*.py "$SRC/parley-who-vertigo" "$SRC/external" "$SRC/minigames" "$SRC/sounds" "$SRC/art" "$DEST/Contents/MacOS"
+OUT="$DIR/tmp"
+
+DEST="$OUT/Parley Who Vertigo.app"
+
+rm -rf "$OUT"
+mkdir -p "$OUT" "$DEST/Contents/MacOS" "$DEST/Contents/Resources"
+
+cp $SRC/README.txt "$OUT"
+cp $SRC/docs/*.pdf "$OUT"
+
+cp "$DIR/macos/Info.plist" "$DEST/Contents"
+cp "$DIR/macos/parleywhovertigo.icns" "$DEST/Contents/Resources"
+
+cp -rpv \
+    "$SRC"/python/parley-who-vertigo \
+    "$SRC"/python/*.py \
+    "$SRC"/python/external \
+    "$SRC"/python/minigames \
+    "$SRC"/python/sounds \
+    "$SRC"/python/art \
+    "$DEST/Contents/MacOS"
+
+cp -rpv \
+    "$DIR"/macos/{bin,lib} \
+    "$DEST/Contents/MacOS/external"
+
+rm -rf "$DEST"/Contents/MacOS/external/{pocketchip,win32}
 find "$DEST" -name __pycache__ -exec rm -rf {} +
-ZIPNAME="$(basename "$DEST" .app).zip"
+ZIPNAME="parley-who-vertigo-$(date +%F)-macos.zip"
 rm -f "$ZIPNAME"
-(cd "$(dirname "$DEST")" && zip -r "$ZIPNAME" "$(basename "$DEST")" *.pdf psmovepair)
+(cd "$OUT" && zip -r ../"$ZIPNAME" *)
+
+rm -rf "$OUT"
+
+mkdir -p "$OUT"
+
+cp $SRC/README.txt "$OUT"
+cp $SRC/docs/*.pdf "$OUT"
+
+cp -rpv \
+    "$SRC"/python/parley-who-vertigo.bat \
+    "$SRC"/python/*.py \
+    "$SRC"/python/external \
+    "$SRC"/python/minigames \
+    "$SRC"/python/sounds \
+    "$SRC"/python/art \
+    "$OUT/"
+
+rm -rf "$OUT"/external/{pocketchip,macos}
+find "$DEST" -name __pycache__ -exec rm -rf {} +
+ZIPNAME="parley-who-vertigo-$(date +%F)-win32.zip"
+rm -f "$ZIPNAME"
+(cd "$OUT" && zip -r ../"$ZIPNAME" *)
+
+rm -rf "$OUT"
+
+mkdir -p "$OUT"
+
+cp $SRC/README.txt "$OUT"
+cp $SRC/docs/*.pdf "$OUT"
+
+cp -rpv \
+    "$SRC"/python/parley-who-vertigo \
+    "$SRC"/python/*.py \
+    "$SRC"/python/external \
+    "$SRC"/python/minigames \
+    "$SRC"/python/sounds \
+    "$SRC"/python/art \
+    "$OUT/"
+
+rm -rf "$OUT"/external/{win32,macos}
+find "$DEST" -name __pycache__ -exec rm -rf {} +
+ZIPNAME="parley-who-vertigo-$(date +%F)-pocketchip.zip"
+rm -f "$ZIPNAME"
+(cd "$OUT" && zip -r ../"$ZIPNAME" *)
+
+rm -rf "$OUT"
